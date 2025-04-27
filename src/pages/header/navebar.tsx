e8ee54291e6c1249174d1b712f689b820edbf20d
ansh bansal <anshbansal811@gmail.com>
2025-04-27 16:03:54 +0530

    make-footer

--- src/pages/header/navebar.tsx
+++ src/pages/header/navebar.tsx
@@ -63,7 +63,6 @@ export const Navbar = () => {
             aria-label="Toggle menu"
           >
             <svg
-              xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
