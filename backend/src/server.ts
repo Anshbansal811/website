2a3f2f19f081cd0de2b1904344b33db1de70b314
ansh bansal <anshbansal811@gmail.com>
2025-05-30 02:26:57 +0530

    add

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,8 +10,8 @@ const port = process.env.PORT || 5000;
 
 // CORS configuration remove if ot work in site
 const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  "http://localhost:5173", // Keep local development working
+  "https://frontend1-t6n0.onrender.com"
+  //"http://localhost:5173", // Keep local development working
 ].filter(Boolean); // Remove any undefined values
 
 app.use(
